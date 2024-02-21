9916897d3c6e2dad6eabbfd93ebd15bfe8b5b48f
Pravin Kori <pravinkori@protonmail.ch>
2024-02-21 13:40:45 +0530

    feat: update font sizes and styles, add new card components with icons
    The font sizes and styles in the CSS file have been updated to improve readability and consistency. Additionally, new card components have been added to the Card.jsx file, each with its own icon and description. These cards provide information about different features of the application, such as monitoring project activity, building teams, evaluating talent, and providing delivery estimates based on past projects.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,50 @@
 import "../css/App.css";
+import iconCalculator from "../assets/images/icon-calculator.svg";
+import iconKarma from "../assets/images/icon-karma.svg";
+import iconSupervisor from "../assets/images/icon-supervisor.svg";
+import iconTeam from "../assets/images/icon-team-builder.svg";
 
 function Card() {
-  return <article>hello</article>;
+  return (
+    <>
+      <div className="wrapper">
+        <div className="layout-grid">
+          <div className="col">
+            <div className="card">
+              <h2>Supervisor</h2>
+              <p>Monitors activity to identify project roadblocks</p>
+              <img src={iconSupervisor} alt="icon" />
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <h2>Team Builder</h2>
+              <p>
+                Scans our talent network to create the optimal team for your
+                project
+              </p>
+              <img src={iconTeam} alt="" />
+            </div>
+            <div className="card">
+              <h2>Karma</h2>
+              <p>Regularly evaluates our talent to ensure quality</p>
+              <img src={iconKarma} alt="" />
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <h2>Calculator</h2>
+              <p>
+                Uses data from past projects to provide better delivery
+                estimates
+              </p>
+              <img src={iconCalculator} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
