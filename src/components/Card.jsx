ba6c0e6f1bdc8060e53b4419ae670d344b76ed41
Pravin Kori <pravinkori@protonmail.ch>
2024-02-21 16:18:05 +0530

    feat: update styles for cards in the application
    - Update the height of the cards to be percentage-based instead of viewport-based
    - Update the color of the text in the body to a lighter shade of grey
    - Add a border color to the cards based on their category (cyan, red, orange, blue)
    - Add a title class to the card titles for consistent styling
    - Add a class to the card images for consistent styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,35 +10,35 @@ function Card() {
       <div className="wrapper">
         <div className="layout-grid">
           <div className="col">
-            <div className="card">
-              <h2>Supervisor</h2>
+            <div className="card border-cyan">
+              <h2 className="card__title">Supervisor</h2>
               <p>Monitors activity to identify project roadblocks</p>
-              <img src={iconSupervisor} alt="icon" />
+              <img className="card__img" src={iconSupervisor} alt="icon" />
             </div>
           </div>
           <div className="col">
-            <div className="card">
-              <h2>Team Builder</h2>
+            <div className="card border-red">
+              <h2 className="card__title">Team Builder</h2>
               <p>
                 Scans our talent network to create the optimal team for your
                 project
               </p>
-              <img src={iconTeam} alt="" />
+              <img className="card__img" src={iconTeam} alt="" />
             </div>
-            <div className="card">
-              <h2>Karma</h2>
+            <div className="card border-orange">
+              <h2 className="card__title">Karma</h2>
               <p>Regularly evaluates our talent to ensure quality</p>
-              <img src={iconKarma} alt="" />
+              <img className="card__img" src={iconKarma} alt="" />
             </div>
           </div>
           <div className="col">
-            <div className="card">
-              <h2>Calculator</h2>
+            <div className="card border-blue">
+              <h2 className="card__title">Calculator</h2>
               <p>
                 Uses data from past projects to provide better delivery
                 estimates
               </p>
-              <img src={iconCalculator} alt="" />
+              <img className="card__img" src={iconCalculator} alt="" />
             </div>
           </div>
         </div>
